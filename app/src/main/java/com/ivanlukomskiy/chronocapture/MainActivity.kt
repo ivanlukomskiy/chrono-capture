1f484d70ca75729937274ef4eb994424a91e83c2
ivanlukomskiy <ivanlukomskiy@avride.ai>
2023-11-30 18:07:17 +0300

    apply

--- app/src/main/java/com/ivanlukomskiy/chronocapture/MainActivity.kt
+++ app/src/main/java/com/ivanlukomskiy/chronocapture/MainActivity.kt
@@ -2,33 +2,36 @@
 
 package com.ivanlukomskiy.chronocapture
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.ivanlukomskiy.chronocapture.ui.theme.ChronoCaptureTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ChronoCaptureTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android", this)
                 }
             }
         }
@@ -36,18 +39,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var text by remember { mutableStateOf("") }
+fun Greeting(name: String, context: Context, modifier: Modifier = Modifier) {
+    val sharedPreferences = context.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+    var text by remember { mutableStateOf(sharedPreferences.getString("cronTab", "") ?: "") }
 
     Column(modifier = modifier) {
-        Text(
-            text = "Hello $name!",
-        )
-        TextField(
-            value = text,
-            onValueChange = { text = it },
-            label = { Text("Enter text") }
-        )
+        Text(text = "Hello $name!")
+
+        Row {
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Image capture cron tab") }
+            )
+
+            Button(onClick = {
+                sharedPreferences.edit().putString("cronTab", text).apply()
+            }) {
+                Text("Apply")
+            }
+        }
     }
 }
 
@@ -55,6 +66,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ChronoCaptureTheme {
-        Greeting("Android")
+        Greeting("Android", LocalContext.current)
     }
 }
